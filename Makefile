440acfc9d95b15e4ed74f42ddeaff51d3430515b
Danil Valetov <danil.valetov@hyperus.team>
2025-04-15 08:56:39 +0300

    MP-10 | feat: makefile and interfaces

---
+++ Makefile
@@ -0,0 +1,22 @@
+TARGET_DIR=.
+
+PYLINT_CONFIG=../.pylintrc
+
+clean:
+	rm -rf logs/*
+
+lint:
+	pylint --rcfile=$(PYLINT_CONFIG) $(TARGET_DIR)
+
+deps:
+	docker pull qdrant/qdrant
+	pip install -r requirements
+
+run:
+	python main.py
+
+qdrant.start:
+	docker run -d -p 6333:6333 -p 6334:6334 --name qdrant qdrant/qdrant
+
+qdrant.stop:
+	docker stop qdrant
---
+++ src/interfaces/interfaces.py
@@ -0,0 +1,74 @@
+"""Модуль для задания базовых интерфейсов"""
+from abc import ABC, abstractmethod
+from typing import Any
+from numpy import ndarray
+from torch import Tensor
+
+
+class IStorage(ABC):
+    """Абстрактный класс векторного хранилища"""
+    @abstractmethod
+    def get_data(self, query_embedding: Tensor | ndarray | list[Tensor],
+                 top_k: int) -> list[Any]:
+        """
+        Получить данные из хранилища
+        :param query_embedding: векторное представление запроса
+        :param top_k: количество лучших чанков
+        :return:
+        """
+
+    @abstractmethod
+    def save_data(self, embeddings: Tensor | ndarray | list[Tensor],
+                  chunks: list[str], metadata: dict):
+        """
+        Сохранить вектора в хранилище
+        :param embeddings: векторное представление чанков
+        :param chunks: чанки текста
+        :param metadata: метаданные чанков
+        :return:
+        """
+    @abstractmethod
+    def delete_by_page_id(self, page_id: int):
+        """
+        Удалить все чанки определенного файла
+        :file_name: имя файла
+        :return:
+        """
+
+
+class IRetriever(ABC):
+    """Абстрактный класс для Ретривера"""
+    @abstractmethod
+    def generate_embeddings(self, chunks: list[str], metadata: list[dict]):
+        """
+        Сгенерировать вектора из кусков текста (стоит добавить опцию нормализацию векторов?)
+        :param chunks:
+        :param metadata:
+        :return:
+        """
+    @abstractmethod
+    def find_similar_context(self, query: str) -> list[(str, str)]:
+        """
+        Парсит в вектора query, затем ищет в storage, ранжирует и отдает возможный контекст
+        :param query:
+        :return:
+        """
+    @abstractmethod
+    def best_match (self, query_list: list[str], context_list: list[(str, str)], top_k: int) -> str:
+        """
+        :param query_list:
+        :param context_list:
+        :param top_k:
+        :return:
+        """
+
+
+class IGenerator(ABC):
+    """Абстрактный класс Генератора"""
+    @abstractmethod
+    def generate_answer(self, query: str) -> str:
+        """
+        Генерирует ответ в LLM
+        :param query:
+        :return:
+        """
