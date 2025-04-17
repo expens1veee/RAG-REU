243cb764a10a1535235014b09a18556bf4ee1eb7
Vadim <vkafanov6@gmail.com>
2025-04-17 14:33:05 +0300

    feat: Dockerfile for app, docker-compose

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+# Используем официальный образ Python
+FROM python:3.11-slim
+
+# Устанавливаем рабочую директорию в контейнере
+WORKDIR /app
+
+# Копируем requirements.txt и устанавливаем зависимости
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Копируем весь проект в контейнер
+COPY . .
+
+# Указываем команду для запуска приложения
+CMD ["python", "main.py"]
--- main.py
+++ main.py
@@ -1,9 +1,34 @@
 from fastapi import FastAPI
 from src.internal.http_server.server import Server
 import uvicorn
+import time
+import os
+import httpx
+
+
+def wait_for_qdrant():
+    qdrant_host = os.getenv("QDRANT_HOST", "localhost")
+    qdrant_port = os.getenv("QDRANT_PORT", "6333")
+    max_attempts = 30
+    attempt = 0
+
+    while attempt < max_attempts:
+        try:
+            with httpx.Client() as client:
+                response = client.get(f"http://{qdrant_host}:{qdrant_port}/")
+                if response.status_code == 200:
+                    print("Qdrant is ready!")
+                    return
+        except httpx.RequestError:
+            attempt += 1
+            print(f"Waiting for Qdrant... Attempt {attempt}/{max_attempts}")
+            time.sleep(2)
+    raise Exception("Qdrant is not available after multiple attempts")
 
 
 def main() -> None:
+    wait_for_qdrant() # Ждем квадрант
+
     # Создаём приложение FastAPI
     app = FastAPI()
 
